75a20d2a054a9beb90c78a1aacd00a599a419867
Octozakt <126351532+Landon-Yt@users.noreply.github.com>
2023-05-29 09:18:01 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+string mathType;
+int firstNumber;
+int secondNumber
+cout << "Multiplication (M) std::endl;
+cout << "Addition? (A) std::endl;
+cout << "Subtraction? (S) std::endl;
+cin >> mathType;
+if(mathType = "M") {
+	cout << "First number?: ";
+	cin >> firstNumber;
+	cout << "Second Number?: ";
+	cin >> secondNumber;
+	cout << "The number is: " << firstNumber * secondNumber;
+
+}
